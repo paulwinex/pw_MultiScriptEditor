187d13f1dae8257cf1462059dd23853c00d0d354
Hasiel Alvarez <hasielhassan@gmail.com>
2016-02-15 16:33:05 -0600

    fixed path to import

--- pw_multiScriptEditor/run_3dsmax.py
+++ pw_multiScriptEditor/run_3dsmax.py
@@ -1,5 +1,5 @@
 import sys, os
-path = os.path.dirname(__file__)
+path = os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) )
 if not path in sys.path:
     sys.path.append(path)
 import pw_multiScriptEditor
