601db43995d2a450d5dc8e9f5b8f0c7ba963d923
paulwinex <paulwinex@gmail.com>
2018-05-11 17:53:23 +0300

    add hqt by default

--- pw_multiScriptEditor/managers/_3dsmax.py
+++ pw_multiScriptEditor/managers/_3dsmax.py
@@ -1,27 +1,34 @@
 import os, sys, re
-
-from PySide import QtGui
-from PySide import QtCore
+try:
+    from PySide.QtGui import *
+    from PySide.QtCore import *
+except:
+    from PySide2.QtGui import *
+    from PySide2.QtCore import *
+    from PySide2.QtWidgets import *
 
 from pw_multiScriptEditor import scriptEditor
 reload(scriptEditor)
 import MaxPlus
-q3dsmax = QtGui.QApplication.instance()
+q3dsmax = QApplication.instance()
 
-class MaxDialogEvents(QtCore.QObject):
+class MaxDialogEvents(QObject):
     def eventFilter(self, obj, event):
         import MaxPlus
-        if event.type() == QtCore.QEvent.WindowActivate:
+        if event.type() == QEvent.WindowActivate:
             MaxPlus.CUI.DisableAccelerators()
-        elif event.type() == QtCore.QEvent.WindowDeactivate:
+        elif event.type() == QEvent.WindowDeactivate:
             MaxPlus.CUI.EnableAccelerators()
-
         return False
 
 def show():
-    se = scriptEditor.scriptEditorClass(parent=MaxPlus.GetQMaxWindow())
-    se.installEventFilter(MaxDialogEvents())
+    try:
+        qtwindow = MaxPlus.GetQMaxWindow()
+    except:
+        qtwindow = MaxPlus.GetQMaxMainWindow()
+    se = scriptEditor.scriptEditorClass(parent=qtwindow)
+    #se.installEventFilter(MaxDialogEvents())
     se.runCommand('import MaxPlus')
-    se.MaxEventFilter = MaxDialogEvents()
-    se.installEventFilter(se.MaxEventFilter)
+    #se.MaxEventFilter = MaxDialogEvents()
+    #se.installEventFilter(se.MaxEventFilter)
     se.show()
